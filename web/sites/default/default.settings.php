3de26f17e610ccee1d06965f24339ef7703498a7
hotgreengem <hotgreengem@gmail.com>
2018-10-25 02:18:19 +0000

    Sync of config

--- web/sites/default/default.settings.php
+++ web/sites/default/default.settings.php
@@ -728,7 +728,10 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * will allow the site to run off of all variants of example.com and
  * example.org, with all subdomains included.
  */
-
+ $settings['trusted_host_patterns'] = array(
+   '^richmond-it\.com.au$',
+   '^.+\.richmond-it\.com.au$',
+ );
 /**
  * The default list of directories that will be ignored by Drupal's file API.
  *
